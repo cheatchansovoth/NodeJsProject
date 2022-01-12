a3ab7c6c2a992449360ea8ce07b94069ce5af192
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-01-12 19:39:14 +1300

    Update Query
    Update Query

---
+++ public/script/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+  $(".info-details h5").hide();
+
+  $(".info-details img").hover(function(){
+  $(".body-content-1 h5").show(1000);});
+
+  $(".body-content-2 img").hover(function(){
+  $(".body-content-2 h5").show(1000);});
+
+  $(".body-content-3 img").hover(function(){
+  $(".body-content-3 h5").show(1000);});
+});
